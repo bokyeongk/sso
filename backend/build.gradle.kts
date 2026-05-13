f9673c853c6b3702975334b663660a392b940d43
kangbokyeong <bokyeong0113@hubilon.com>
2026-05-13 17:26:04 +0900

    refactor: spring boot v3 -> v4

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.4.4"
+    id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -30,14 +30,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
     /* 1.0.6 회원가입 추가 */
     /* 1.0.7 아이디/이메일 조회(중복확인) 추가 */
     /* 1.0.8 로그인 및 회원가입 메소드화 */
     /* 1.0.9 에러로그 추가 */
     /* 1.0.10 dynamic scope */
     /* 1.0.11 user 조회 method 추가 */
-    implementation(files("libs/common-auth-lib-1.0.11.jar"))
+//    implementation(files("libs/common-auth-lib-1.0.11.jar"))
+
+    /* 2.0.0 springboot 4버전 */
+    implementation(files("libs/common-auth-lib-2.0.0.jar"))
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("org.springframework.boot:spring-boot-starter-security")
 
@@ -48,6 +51,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
